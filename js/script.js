50b4ee87592ee70a90b1c6968982835b2fd05a5d
SimoneFranzoni <simofranzoni99@gmail.com>
2021-10-27 18:01:13 +0200

    first-commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/*
+const images = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+*/
+
+const item = document.getElementsByClassName('item');  
+
+let cont = 0;
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+
+next.addEventListener('click',function(){
+    const++;
+    if(cont )
+});
+
+prev.addEventListener('click',function(){
+    const--;
+    if(cont < 0) contatore = item.lenght--;
+});
